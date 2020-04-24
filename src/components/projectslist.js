c8aad07adc3177aa1d314f313e9ae57403dcdd6c
Vavarooks <vavarooks123@gmail.com>
2020-04-23 17:18:07 -0700

    images

--- src/components/projectslist.js
+++ src/components/projectslist.js
@@ -8,27 +8,35 @@ var projectlist=[
 
     },
     {
-        title:"Password Generator",
-        description: "Generates Password with Javascript.",
-        github:"https://github.com/Vavarooks/Homework3",
-        deployed:"https://vavarooks.github.io/Homework3/Front.html",
-        img:"./assets/password.jpg"
+        title:"Quiz Generator",
+        description: "Generates a Quiz with Javascript.",
+        github:"https://github.com/Vavarooks/Homework4",
+        deployed:"https://vavarooks.github.io/Homework4/Home.html",
+        img:"./assets/quiz.jpg"
 
     },
     {
-        title:"Password Generator",
-        description: "Generates Password with Javascript.",
-        github:"https://github.com/Vavarooks/Homework3",
-        deployed:"https://vavarooks.github.io/Homework3/Front.html",
-        img:"./assets/password.jpg"
+        title:"Daily Planner",
+        description: "A basic everyday planner for schedualing daily events.",
+        github:"https://github.com/Vavarooks/Homework5",
+        deployed:"https://vavarooks.github.io/Homework5/frontpage.html",
+        img:"./assets/planner.jpg"
 
     },
     {
-        title:"Password Generator",
-        description: "Generates Password with Javascript.",
-        github:"https://github.com/Vavarooks/Homework3",
-        deployed:"https://vavarooks.github.io/Homework3/Front.html",
-        img:"./assets/password.jpg"
+        title:"Weather App",
+        description: "A basic weather app that gathers the weather from the city and state inputed for a weekly forcast. It also obtains the UV index and shows current weather for the day. There are also functions to get to the past search results.",
+        github:"https://github.com/Vavarooks/Homework6",
+        deployed:"https://vavarooks.github.io/Homework6/",
+        img:"./assets/weather.jpg"
+
+    },
+    {
+        title:"Team Builder",
+        description: "A basic weather app that gathers the weather from the city and state inputed for a weekly forcast. It also obtains the UV index and shows current weather for the day. There are also functions to get to the past search results.",
+        github:"https://github.com/Vavarooks/Homework10",
+        deployed:"https://vavarooks.github.io/Homewrok10/index.html",
+        img:"./assets/team.jpg"
 
     }
 ]
