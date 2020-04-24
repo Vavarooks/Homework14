341545e5baa2bbc3040812aa1babc19228bea283
Vavarooks <vavarooks123@gmail.com>
2020-04-24 12:15:23 -0700

    Updates

---
+++ precache-manifest.feed642be21956cc578e2fed78561c06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0331c077e9dabf236b82802e0343ab8",
+    "url": "/Vavarooks/Homework14/index.html"
+  },
+  {
+    "revision": "90c470c630429c39042f",
+    "url": "/Vavarooks/Homework14/static/css/main.da2cb113.chunk.css"
+  },
+  {
+    "revision": "2d60f2c2f3590dc423d5",
+    "url": "/Vavarooks/Homework14/static/js/2.b74b505d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Vavarooks/Homework14/static/js/2.b74b505d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90c470c630429c39042f",
+    "url": "/Vavarooks/Homework14/static/js/main.1ae94c66.chunk.js"
+  },
+  {
+    "revision": "4749482a9ad2bf2fe600",
+    "url": "/Vavarooks/Homework14/static/js/runtime-main.2a4a2a8f.js"
+  }
+]);
