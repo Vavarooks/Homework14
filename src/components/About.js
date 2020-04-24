4c2a9118b750ee985f70e69eec7f4e447b300884
Vavarooks <vavarooks123@gmail.com>
2020-04-24 12:08:23 -0700

    edits of more colors and syles

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ function About(){
     <div className="container">
 <h1>About</h1>
 <br/>
-<p>I was born July 4, 2001 in California. I was raised in San Mateo for most my youth. My family moved in 2008
+<p className="bio">I was born July 4, 2001 in California. I was raised in San Mateo for most my youth. My family moved in 2008
             because of the banking crisis, I learned this later. We moved to Washington State, Kennewick. There I spent
             most my teen years, going back and forth between Washington and California. In 2013-15 we moved back, I
             don’t remember the exact year. I have ben living in Richmond for six years now. I like to play video games
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -4,12 +4,11 @@ function Contacts(){
     return(<div className="container">
 <h1>Contacts</h1>
 <div className="container contacts">
+<a href="mailto:vavarooks123@gmail.com"><i className="fa fa-envelope fa-1x"></i> Email</a>
 <br/>
-<a href="mailto:vavarooks123@gmail.com" >Email</a>
+<a target="_blank" rel="noopener noreferrer" href="https://github.com/Vavarooks"><i className="fa fa-github fa-1x"></i> Github</a>
 <br/>
-<a target="_blank" rel="noopener noreferrer" href="https://github.com/Vavarooks">Github</a>
-<br/>
-<a target="_blank" rel="noopener noreferrer" href="www.linkedin.com/in/vanessa-emerick">Linkedin</a>
+<a target="_blank" rel="noopener noreferrer" href="www.linkedin.com/in/vanessa-emerick"><i className="fa fa-linkedin fa-1x"></i> Linkedin</a> 
 </div>
     </div>      
     )
